7b671445b0f77f0f720a369b84dcbe402713d6ae
Savas <savas.neto20@gmail.com>
2025-10-09 12:37:35 -0300

    fix random errors

--- src/view/MenuPrincipal.java
+++ src/view/MenuPrincipal.java
@@ -1,29 +1,7 @@
 package view;
 
-import controller.AlunoController;
-
-import java.util.Scanner;
-
 public class MenuPrincipal {
     public static void main(String[] args) {
-        AlunoController alunoController = new AlunoController();
-        Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("\n=== MENU BIBLIOTECA ===");
-            System.out.println("1 - Listar Alunos");
-            System.out.println("0 - Sair");
-            System.out.print("Escolha: ");
-            int opcao = sc.nextInt();
-
-            switch (opcao) {
-                case 1 -> alunoController.listarAlunos();
-                case 0 -> {
-                    System.out.println("Encerrando...");
-                    return;
-                }
-                default -> System.out.println("Opção inválida!");
-            }
-        }
     }
 }
