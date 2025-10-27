a683f5698f8e7ec203c11b88dc56d0f66f4b779a
Savas Neto <savas.neto20@gmail.com>
2025-10-27 20:08:01 -0300

    Remove LIVRO and EMPRESTIMO table definitions
    Removed the creation of LIVRO and EMPRESTIMO tables from the script.

--- script.sql
+++ script.sql
@@ -11,16 +11,6 @@ CREATE DATABASE devlibrary;
 		genero VARCHAR(50),
 		status ENUM('Disponível', 'Emprestado') DEFAULT 'Disponível'
 	);
-	-- LIVROS
-	CREATE TABLE LIVRO (
-		id_livro INT AUTO_INCREMENT PRIMARY KEY,
-		titulo VARCHAR(150) NOT NULL,
-		isbn CHAR(13) NOT NULL UNIQUE,
-		autor VARCHAR(100) NOT NULL,
-		ano_publicacao YEAR,
-		genero VARCHAR(50),
-		status ENUM('Disponível', 'Emprestado') DEFAULT 'Disponível'
-	);
 
 	-- LEITORES
 	CREATE TABLE LEITOR (
@@ -40,19 +30,6 @@ CREATE DATABASE devlibrary;
 		telefone VARCHAR(15)
 	);
 
-	-- EMPRÉSTIMOS
-	CREATE TABLE EMPRESTIMO (
-		id_emprestimo INT AUTO_INCREMENT PRIMARY KEY,
-		fk_livro INT NOT NULL,
-		fk_leitor INT NOT NULL,
-		fk_funcionario INT NOT NULL,
-		data_emprestimo DATE NOT NULL,
-		data_prevista_devolucao DATE NOT NULL,
-		data_devolucao DATE,
-		CONSTRAINT fk_emp_livro FOREIGN KEY (fk_livro) REFERENCES LIVRO(id_livro),
-		CONSTRAINT fk_emp_leitor FOREIGN KEY (fk_leitor) REFERENCES LEITOR(id_leitor),
-		CONSTRAINT fk_emp_func FOREIGN KEY (fk_funcionario) REFERENCES FUNCIONARIO(id_funcionario)
-	);
 	-- EMPRÉSTIMOS
 	CREATE TABLE EMPRESTIMO (
 		id_emprestimo INT AUTO_INCREMENT PRIMARY KEY,
